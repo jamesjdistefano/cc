0b0c3a3d4ae71ca4591e5e0467d5b62373f700b6
jamesjdistefano <jamesjdistefano@gmail.com>
2023-03-13 14:30:15 -0400

    Adds gridsome site + initial content

---
+++ src/components/Header.vue
@@ -0,0 +1,204 @@
+<template>
+  <div :style="variation">
+    <div
+      class="header"
+      :style="{
+        'background-color': bgColor,
+        'border-bottom': borderColor + ' solid 8px',
+        padding: padding,
+      }">
+
+      <div class="bgImage" :style="{'background-image': bgImage}"></div>
+
+      <div class="content" :style= "{ 'background-color': colColor }">
+
+        <span  v-if="this.$slots.label" class="label"><slot name="label">Label</slot></span>
+
+        <h1 class="heading1">
+          <slot>Here we have a page title</slot>
+        </h1>
+
+        <p v-if="this.$slots.support" class="support display">
+          <slot name="support">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </slot>
+        </p>
+
+        <div  v-if="this.$slots.context" class="context">
+          <slot name="context">Context</slot>
+        </div>
+      </div>
+
+    </div>
+
+    <div v-if="this.$slots.intro" class="intro">
+      <p class="intro-txt">
+        <slot name="intro"></slot>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    align: {
+      type: String,
+    },
+    variation: {
+      type: String,
+    },
+    bgColor: {
+      type: String,
+    },
+    colColor: {
+      type: String,
+    },
+    borderColor: {
+      type: String,
+    },
+    bgImage: {
+      type: String,
+    },
+    padding: {
+      String,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  @import "../assets/css/variables.scss";
+
+.header {
+  padding: 0rem 5rem;
+  min-height: 40rem;
+  width: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  overflow: hidden;
+  position: relative;
+
+  .bgImage {
+    position: absolute;
+    top: -10%; left: -10%;
+    width: 150%; height: 150%;
+    background-position: center;
+    background-size: cover;
+    filter: blur(8px);
+    z-index: 0;
+  }
+
+  .label {
+    margin-bottom: 3rem;
+  }
+
+  .title {
+    margin-top: 0;
+    margin-bottom: 0rem;
+  }
+
+  .content {
+    padding: 3rem;
+    max-width: 80rem;
+    z-index: 1;
+    position: relative;
+  }
+
+  .context {
+    font-size: 1.4rem;
+    margin-top: 3rem;
+    line-height: clamp(1.6rem, 5vw, 2.4rem);
+    border-left: 3px $light-blue solid;
+    padding-left: 1.6rem;
+    max-width: 64rem;
+  }
+
+  @media (max-width: 800px) {
+    padding: 2rem;
+  }
+}
+
+.intro {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding:0 3rem;
+  background: rgba(255, 255, 255, 0.9);
+}
+
+.intro-txt {
+  color: $med-blue;
+  text-align: left;
+  max-width: 80rem;
+  font-size: 4rem;
+  line-height: 6rem;
+}
+
+.v1 {
+  .header {
+    background-size: cover;
+  }
+}
+
+.v2 {
+  .content {
+    background-size: contain;
+  }
+
+  img {
+    display: block;
+  }
+}
+
+.v3 {
+  .header {
+    background-position: center;
+  }
+
+  .content {
+    padding: 5rem 5rem;
+  }
+
+  .support {
+    line-height: clamp(2.6rem, 7vw, 4rem);
+  }
+}
+
+.v4 {
+  .header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .content {
+    max-width: 90rem;
+    min-height: 36rem;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+}
+
+.v5 {
+  .content {
+    max-width: 120rem;
+  }
+}
+
+@media (max-width: 800px) {
+
+  .v3 {
+    .content {
+      padding: 1rem;
+    }
+  }
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,93 @@
+<template>
+  <HomeLayout>
+
+    <Header bg-color="var(--light-blue)" variation = "v2" colColor="var(--lavender)" border-color="var(--med-blue)" padding="8rem" bgImage="url('../../images/dall-e-2.png')">
+      <template>Chris Clark<br />Loan Officer</template>
+      <template #support>Lugging this fat cock uphill since 1986.</template>
+      <template #context>
+        <ul class="contact-info">
+          <li>NMLS# 2383562</li>
+          <li><a href="tel:7704014199">770-401-4199</a></li>
+          <li><a href="mailto:CC@ybilending.com">CC@ybilending.com</a></li>
+        </ul>
+      </template>
+    </Header>
+    
+    <div class="fourteenfour">
+      <h2 class="align-left">For my Film Industry friends</h2>
+      <p class="align-left display">Please take a look below at these helpful links; they really helped with my home buying. </p>
+      <ul class="copy align-left">     
+        <li><a href="https://play.google.com/store/apps/details?id=com.thegrizzlylabs.geniusscan.free&hl=en_US&gl=US&pli=1">Genius Document Scanning Android</a ></li>
+        <li><a href="https://dl.tglapp.com/genius-scan">Genius Document Scanning Apple</a></li>
+        <li><a href="https://www.castandcrew.com/blog/employment-income-verification/">Cast and Crew Verification Of Employment link</a></li>
+        <li>Entertainment Partners Verification Of Employment: <a href="tel:8185994449">818-955-4449</a>, <a href="mailto:uigroup@ep.com">uigroup@ep.com</a></li>
+      </ul>
+    </div>
+    
+
+    <div class="tenhundo">
+      <div class="grid">
+        <div class="item"><img src="../../static/images/dall-e-1.png" width="300" /></div>
+        <div class="item"><img src="../../static/images/dall-e-3.png" width="300" /></div>
+        <div class="item"><img src="../../static/images/dall-e-4.png" width="300" /></div>
+      </div>
+      Copyright 2023, Chris Clark, Loan Officer
+    </div>
+    
+    </HomeLayout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Chris Clark, Loan Officer. Call me at 770-401-4199'
+  }
+}
+</script>
+
+
+
+<style scoped lang="scss">
+// @import "../../assets/css/variables.scss";
+
+
+h2 { 
+  margin-top: 8rem; 
+  margin-bottom: 0rem;
+  font-size: 4rem;
+}
+
+h3 { 
+  margin-bottom: 0rem;
+  font-size: 2.8rem;
+}
+
+
+li {
+  padding-left: 1.6rem;
+  margin-left: -5.7rem;
+  list-style-type: none;
+}
+
+.grid {
+  display: grid;
+  max-width: 100rem;
+  row-gap: 0px;
+  column-gap: 3px;
+  margin-bottom: 12rem;
+}
+
+
+
+
+@media (min-width: 600px) {
+ .grid { grid-template-columns: repeat(2, 1fr); }
+}
+
+
+@media (min-width: 900px) {
+  .grid { grid-template-columns: repeat(3, 1fr); }
+}
+
+
+</style>
